ef47b0ce48f5e6c19f3e86e86c20f5a61b855c97
DevenNan <devenamazingnandapurkar@gmail.com>
2023-01-24 13:30:40 +0530

    Updates

---
+++ precache-manifest.7cec1b30156309883a1a5c260379546f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca4596f50b5cb71435ecdf5540f6fde",
+    "url": "/trvl-partner/index.html"
+  },
+  {
+    "revision": "71b000cd4718a5822325",
+    "url": "/trvl-partner/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/trvl-partner/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trvl-partner/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b000cd4718a5822325",
+    "url": "/trvl-partner/static/js/main.3a715467.chunk.js"
+  },
+  {
+    "revision": "057f184aadc9e9167e42",
+    "url": "/trvl-partner/static/js/runtime-main.a00b531b.js"
+  }
+]);
